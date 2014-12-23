87636ce6946675ba1e3dafe9cc57cbb084f41936
Keita Yamamoto <kurometiko@gmail.com>
2014-12-23 18:26:20 +0900

    ネットワークインターフェイスの作成
    ネットワークプロトコルの作成完了
    
    socketインターフェイスの作成
    
    設定ファイルの修正

---
+++ evolution_network_num.h
@@ -0,0 +1,134 @@
+﻿#ifndef __EVOLUTION_NETWORK_NUM_H_
+#define __EVOLUTION_NETWORK_NUM_H_
+
+#include <evolution.h>
+
+namespace EVOLUTION{
+    namespace NETWORK{
+
+        /*
+        _UNSPEC = 0, // unspecified
+        _UNIX = 1, // local to host (pipes, portals)
+        _INET = 2, // internetwork: UDP, TCP, etc.
+        _IMPLINK = 3, // arpanet imp addresses
+        _PUP = 4, // pup protocols: e.g. BSP
+        _CHAOS = 5, // mit CHAOS protocols
+        _NS = 6, // XEROX NS protocols
+        _IPX = _NS, // IPX protocols: IPX, SPX, etc.
+        _ISO = 7, // ISO protocols
+        _OSI = _ISO, // OSI is ISO
+        _ECMA = 8, // european computer manufacturers
+        _DATAKIT = 9, // datakit protocols
+        _CCITT = 10, // CCITT protocols, X.25 etc
+        _SNA = 11, // IBM SNA
+        _DECnet = 12, // DECnet
+        _DLI = 13, // Direct data link interface
+        _LAT = 14, // LAT
+        _HYLINK = 15, // NSC Hyperchannel
+        _APPLETALK = 16, // AppleTalk
+        _NETBIOS = 17, // NetBios-style addresses
+        _VOICEVIEW = 18, // VoiceView
+        _FIREFOX = 19, // Protocols from Firefox
+        _UNKNOWN1 = 20, // Somebody is using this!
+        _BAN = 21, // Banyan
+        _ATM = 22, // Native ATM Services
+        _INET6 = 23, // Internetwork Version 6
+        _CLUSTER = 24, // Microsoft Wolfpack
+        _12844 = 25, // IEEE 1284.4 WG AF
+        _IRDA = 26, // IrDA
+        _NETDES = 28, // Network Designers OSI & gateway
+        */
+        struct ADDRESSFAMILY {
+            enum _ADDRESSFAMILY {
+                _UNSPEC = 0, // unspecified
+                _UNIX = 1, // local to host (pipes, portals)
+                _INET = 2, // internetwork: UDP, TCP, etc.
+                _IMPLINK = 3, // arpanet imp addresses
+                _PUP = 4, // pup protocols: e.g. BSP
+                _CHAOS = 5, // mit CHAOS protocols
+                _NS = 6, // XEROX NS protocols
+                _IPX = _NS, // IPX protocols: IPX, SPX, etc.
+                _ISO = 7, // ISO protocols
+                _OSI = _ISO, // OSI is ISO
+                _ECMA = 8, // european computer manufacturers
+                _DATAKIT = 9, // datakit protocols
+                _CCITT = 10, // CCITT protocols, X.25 etc
+                _SNA = 11, // IBM SNA
+                _DECnet = 12, // DECnet
+                _DLI = 13, // Direct data link interface
+                _LAT = 14, // LAT
+                _HYLINK = 15, // NSC Hyperchannel
+                _APPLETALK = 16, // AppleTalk
+                _NETBIOS = 17, // NetBios-style addresses
+                _VOICEVIEW = 18, // VoiceView
+                _FIREFOX = 19, // Protocols from Firefox
+                _UNKNOWN1 = 20, // Somebody is using this!
+                _BAN = 21, // Banyan
+                _ATM = 22, // Native ATM Services
+                _INET6 = 23, // Internetwork Version 6
+                _CLUSTER = 24, // Microsoft Wolfpack
+                _12844 = 25, // IEEE 1284.4 WG AF
+                _IRDA = 26, // IrDA
+                _NETDES = 28, // Network Designers OSI & gateway
+            };
+        };
+
+        /*
+         RECEIVE = 0x00, //SHUT_RD
+         SEND = 0x01,    //SHUT_WR
+         BOTH = 0x02,    //SHUT_RDWR
+         NONE = 0xFF,    //SHUT_RDWR
+         */
+        struct SHUTDOWN{
+            enum _SHUTDOWN{
+                RECEIVE = 0x00, //SHUT_RD
+                SEND = 0x01,    //SHUT_WR
+                BOTH = 0x02,    //SHUT_RDWR
+                NONE = 0xFF,    //SHUT_RDWR
+            };
+        };
+
+        struct BLOCKINGMODE{
+            enum _BLOCKINGMODE{
+                BLOCK = 0x00, //BLOCK
+                NOBLOCK = 0x01,    //NOBLOCK
+            };
+        };
+
+        struct IPADDR{
+            union{
+                u8 u_addr_b[4];
+                u16 u_addr_s[2];
+                u32 u_addr;
+            }Addr;
+        };
+
+        struct SOCKET_DESC{
+            IPADDR ip;
+            u16    port;
+            ADDRESSFAMILY::_ADDRESSFAMILY addr_family;
+        };
+
+#define EVOLUTION_IP_SET(ip1,ip2,ip3,ip4) ((ip4 << 24) | (ip3 << 16) | (ip2 << 8) | (ip1))
+
+        EVOLUTION_INLINE u32 IP_Convert(u8 ip1, u8 ip2, u8 ip3, u8 ip4){
+            return ((ip4 << 24) | (ip3 << 16) | (ip2 << 8) | (ip1));
+        }
+
+        //ファイルディスクリプタの最大数
+#if defined(_WIN32) || defined(_WIN64) 
+        const u32 FILE_DESCRIPTOR_SIZE = 65536;
+#elif defined(_LINUX)
+        const u32 FILE_DESCRIPTOR_SIZE = FD_SETSIZE;
+#endif
+
+#ifndef DEF_SOCKET
+#define DEF_SOCKET
+        typedef EVOLUTION::u32 SOCKET;
+#endif
+
+    }
+}
+
+
+#endif //!__EVOLUTION_NETWORK_NUM_H_
---
+++ protocol/evolution_network_protocol.cpp
@@ -0,0 +1,96 @@
+﻿#include "../evolution_network_implement.h"
+
+using namespace EVOLUTION;
+using namespace EVOLUTION::NETWORK;
+
+
+//IUnknown
+u32 Protocol::AddRef(){
+    return m_instance_counter.AddRef();
+}
+
+RESULT Protocol::QueryInterface(EVOLUTION::EVOLUTION_IID riid, void **ppvObject){
+    if (IsEqualGUID(riid, EVOLUTION_GUID::IID_IUnknown))
+    {
+        *ppvObject = static_cast<IUnknown *>(this);
+        this->AddRef();
+    }
+    else if (IsEqualGUID(riid, EVOLUTION_GUID::IID_IProtocol))
+    {
+        *ppvObject = static_cast<IProtocol *>(this);
+        this->AddRef();
+    }
+    else if (IsEqualGUID(riid, EVOLUTION_GUID::IID_Protocol))
+    {
+        *ppvObject = static_cast<Protocol *>(this);
+        this->AddRef();
+    }
+    else
+    {
+        *ppvObject = nullptr;
+        return RESULT::E_no_instance;
+    }
+    return RESULT::S_ok;
+}
+
+u32 Protocol::Release(){
+    u32 counter = this->m_instance_counter.Release();
+    if (counter == 0){
+        delete this;
+    }
+    return counter;
+}
+
+Protocol::Protocol(){
+
+}
+
+Protocol::~Protocol(){
+
+}
+
+NetworkResult::_RESULT Protocol::Create(const c8* host_name, u16 port, ADDRESSFAMILY::_ADDRESSFAMILY addr_family){
+    if (EVOLUTION::FUNCTION::GetAddress(this->m_desc.ip, host_name))
+    {
+        return NetworkResult::GET_IPADDR_FAILED;
+    }
+    this->m_desc.port = port;
+    this->m_desc.addr_family = addr_family;
+    return NetworkResult::RESULT_OK;
+}
+
+NetworkResult::_RESULT Protocol::Create(IPADDR ip, u16 port, ADDRESSFAMILY::_ADDRESSFAMILY addr_family){
+    this->m_desc.ip = ip;
+    this->m_desc.port = port;
+    this->m_desc.addr_family = addr_family;
+    return NetworkResult::RESULT_OK;
+}
+
+NetworkResult::_RESULT Protocol::Create(const SOCKET_DESC& desc){
+    this->m_desc.ip = desc.ip;
+    this->m_desc.port = desc.port;
+    this->m_desc.addr_family = desc.addr_family;
+    return NetworkResult::RESULT_OK;
+}
+
+//プロトコルを取得します。
+void Protocol::GetDesc(SOCKET_DESC* desc)const{
+    desc->ip = this->m_desc.ip;
+    desc->port = this->m_desc.port;
+    desc->addr_family = this->m_desc.addr_family;
+}
+
+//IPアドレスを取得します。
+const IPADDR& Protocol::GetipAddr()const{
+    return this->m_desc.ip;
+}
+
+//ポート番号を取得します。
+u16 Protocol::GetPort()const{
+    return this->m_desc.port;
+}
+
+//アドレスファミリーを取得します。
+ADDRESSFAMILY::_ADDRESSFAMILY Protocol::GetAddrFamily()const{
+    return this->m_desc.addr_family;
+}
